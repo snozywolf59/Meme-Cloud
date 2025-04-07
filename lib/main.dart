50d4580de6774623cb62a3d47115c737bdf367f0
Bang <thbang727@gmail.com>
2025-04-07 10:41:53 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:meme_cloud/firebase_options.dart';
 
 import 'package:meme_cloud/presentation/view/dashboard.dart';
 import 'package:meme_cloud/presentation/view/start_view.dart';
@@ -6,12 +8,19 @@ import 'package:provider/provider.dart';
 
 import 'package:meme_cloud/core/configs/theme/theme_provider.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    child: const MyApp(),
-  ),
-);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

2051ace22de89de7005d00a6388fbfb9ce98e893
Bang <thbang727@gmail.com>
2025-04-12 17:58:05 +0700

    env

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:meme_cloud/firebase_options.dart';
 
 import 'package:meme_cloud/presentation/view/dashboard.dart';
 import 'package:meme_cloud/presentation/view/start_view.dart';
 import 'package:meme_cloud/service_locator.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart' as provider_package;
 
 import 'package:meme_cloud/core/configs/theme/theme_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
+  print( dotenv.env['SUPABASE_URL']);
+  print( dotenv.env['SUPABASE_ANON_KEY']);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Supabase.initialize(
+    url: 'https://jvqlqzqkqzqjvqlqzqkq.supabase.co',
+    anonKey: 'aaaa'
+  );
   await initDependencies();
   runApp(
-    ChangeNotifierProvider(
+    provider_package.ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
       child: const MyApp(),
     ),
@@ -29,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meme Cloud',
-      theme: Provider.of<ThemeProvider>(context).themeData,
+      theme: provider_package.Provider.of<ThemeProvider>(context).themeData,
       home: true == false ? DashBoard() : const StartView(),
       debugShowCheckedModeBanner: false,
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import firebase_auth
 import firebase_core
+import path_provider_foundation
+import shared_preferences_foundation
+import sign_in_with_apple
+import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }

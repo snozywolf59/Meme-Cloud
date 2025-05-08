7a395be5d5dda4825505dd27a1e4e0f49ce93a18
To Huu Bang <120768537+snozywolf59@users.noreply.github.com>
2025-05-08 22:19:37 +0700

    Delete lib/domain directory

--- lib/domain/repositories/profile/user_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-abstract class UserRepository {
-  Future<Either> getCurrentUser();
-
-  Future<Either> changePassword(String newPassword);
-
-  Future<String?> getAvatarUrl();
-}
